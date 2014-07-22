10345775e4e501cf8d4238a2ecb984674a3220c5
your_name <your_email>
2014-07-22 21:15:38 +0800

    is_not_signing_up通过

--- data_structure_1/src/notify_sms_received.js
+++ data_structure_1/src/notify_sms_received.js
@@ -1,13 +1,25 @@
-function notify_sms_received(sms_json){
+function notify_sms_received(sms_json) {
     var message = sms_json.messages[0].message.replace(/\s/g, "");
-    if(message.search(/bm/i) == 0){
+    if (message.search(/bm/i) == 0) {
         var activities = JSON.parse(localStorage.activities);
         var name = message.substr(2);
         var phone = sms_json.messages[0].phone;
-        var even = _.find(activities,function(activity){return activity.name == localStorage.current_activity});
-        if(!_.find(even.sign_ups,function(sign_up){return sign_up.phone == phone})){
-            even.sign_ups.unshift({'name':name,'phone':phone});
-            localStorage.setItem('activities',JSON.stringify(activities));
+        var even = _.find(activities, function (activity) {
+            return activity.name == localStorage.current_activity
+        });
+        if (localStorage.is_signing_up) {
+            if (!_.find(even.sign_ups, function (sign_up) {
+                return sign_up.phone == phone
+            })) {
+                even.sign_ups.unshift({'name': name, 'phone': phone});
+            }
         }
+        if (!localStorage.is_signing_up || localStorage.is_signing_up == "false") {
+            even.sign_ups = [];
+            console.log(even.sign_ups.length)
+        }
+        localStorage.setItem('activities', JSON.stringify(activities));
     }
+
+
 }
