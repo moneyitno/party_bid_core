d0a342b1b65d53b4643a600addd77ae4f050a543
your_name <your_email>
2014-07-23 23:27:25 +0800

    data3 show all signed up users

--- data_structure_3/src/render.js
+++ data_structure_3/src/render.js
@@ -15,3 +15,8 @@ render_biddings = function (current_id, bidName) {
     }).biddings
 
 };
+
+render_sign_ups = function (current_id) {
+    var sign_ups = JSON.parse(localStorage.sign_ups);
+    return _.filter(sign_ups,function(sign_up){return sign_up.activity_id == current_id})
+};
