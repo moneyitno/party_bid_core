ec2d49c67dba4e4233124883833e930f1d62a7ae
your_name <your_email>
2014-07-23 15:03:04 +0800

    data2_sign_up_successfully_when_it_is_signing_up

--- data_structure_2/src/notify_sms_received.js
+++ data_structure_2/src/notify_sms_received.js
@@ -1,3 +1,20 @@
-function notify_sms_received(){
-
+function notify_sms_received(sms_json) {
+    var message = sms_json.messages[0].message.replace(/\s/g, "");
+    var activities = JSON.parse(localStorage.activities);
+    var phone = sms_json.messages[0].phone;
+    sign_up_message(message, phone, activities);
 }
+
+sign_up_message = function (message, phone, activities) {
+    if (message.search(/bm/i) == 0) {
+        var name = message.substr(2);
+        signing_up(phone, name, activities);
+        localStorage.setItem('activities', JSON.stringify(activities));
+    }
+};
+
+signing_up = function (phone, name, activities) {
+    if (localStorage.is_signing_up) {
+        activities[localStorage.current_activity_id].sign_ups.unshift({'name': name, 'phone': phone});
+    }
+};
