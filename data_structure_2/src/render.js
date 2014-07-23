06f787cbec710ab48742303be27c8134395b6971
your_name <your_email>
2014-07-23 17:04:27 +0800

    data2_show_all_signed_up_users

--- data_structure_2/src/render.js
+++ data_structure_2/src/render.js
@@ -3,7 +3,12 @@ transform_bids_to_view_model = function (current_id) {
     return activities[current_id].bids
 };
 
-transform_biddings_to_view_model = function (current_id,bidName){
+transform_biddings_to_view_model = function (current_id, bidName) {
     var activities = JSON.parse(localStorage.activities);
     return activities[current_id].biddings[bidName]
+};
+
+render_sign_ups = function (current_id) {
+    var activities = JSON.parse(localStorage.activities);
+    return activities[current_id].sign_ups
 };
